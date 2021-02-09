4d6797e5c90371dcea5fdf1546b0d1b0a7a9e8fc
chand1012 <chandler@chand1012.dev>
2021-02-09 16:37:42 -0500

    Add post test

--- handler.js
+++ handler.js
@@ -10,3 +10,12 @@ module.exports.endpoint = (event, context, callback) => {
 
   callback(null, response);
 };
+
+module.exports.posttest = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(event)
+  };
+
+  callback(null, response);
+};
